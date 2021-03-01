79acc0faa485889f3ac03c74f2cb5f6133fff706
AMOGH <hanumanprasadkp@gmail.com>
2021-03-01 18:05:55 +0530

    sling

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,9 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage('sprites/sling1.png')
+        this.sling2=loadImage('sprites/sling2.png') 
+        this.sling3=loadImage('sprites/sling3.png')
     }
 
     fly(){
@@ -16,12 +19,29 @@ class SlingShot{
     }
 
     display(){
+        image(this.sling1,200,20)
+        image(this.sling2,169,20)
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-        }
+           // strokeWeight(4);
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            stroke(48,22,8);
+            if(pointA.x<200){
+            strokeWeight(10);
+            line(pointA.x-10,pointA.y,pointB.x+40,pointB.y);
+            line(pointA.x-10,pointA.y,pointB.x-5,pointB.y+20);
+            image(this.sling3,pointA.x-30,pointA.y,20,20)
+            }
+            else{
+                strokeWeight(5);
+                line(pointA.x-10,pointA.y,pointB.x+40,pointB.y);
+                line(pointA.x-10,pointA.y,pointB.x-5,pointB.y+20);
+                image(this.sling3,pointA.x-30,pointA.y,20,20)
+            }
+            pop();
+        } 
     }
     
 }
